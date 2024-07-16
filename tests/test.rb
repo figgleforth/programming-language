cb30fcd4cfd9674e02fdebc05b1b8eb5e88c634e
Bojan Percevic <422873+figgleforth@users.noreply.github.com>
2024-07-16 15:37:18 -0400

    Added parsing live file stage to test.rb

--- tests/test.rb
+++ tests/test.rb
@@ -4,8 +4,12 @@ require 'benchmark'
 puts
 
 Benchmark.bm(12) do |x|
-    x.report('parser:') { require_relative 'parser' }
-    x.report('interpreter:') { require_relative 'interpreter' }
+    x.report('parser') { require_relative 'parser' }
+    x.report('interpreter') { require_relative 'interpreter' }
+    x.report('sandbox em') do
+        tokens = Lexer.new(File.read('tests/sandbox.em').to_s).lex
+        ast    = Parser.new(tokens).to_ast
+    end
 end
 
 puts "\nPassed #{@tests_ran} tests"
