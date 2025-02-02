eeff564367c4bd80827ab7f778a29bd8523cb86b
Bojan <422873+figgleforth@users.noreply.github.com>
2025-02-02 13:20:56 -0500

    Added new examples
    String and Float

---
+++ examples/float.f
@@ -0,0 +1,16 @@
+Float {}
+
+cd Float `Don't waste space indenting, enter the scope
+
+MAX =  inf `constants are capitalized
+MIN = -inf
+
+abs {;} `make your declarations for Float
+
+if some_condition
+	`declarations
+else
+	`declarations
+end `use `end` or `}`
+
+cd .. `exit Float
